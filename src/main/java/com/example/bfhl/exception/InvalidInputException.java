fa01dc2a333ab777e6b45f93a5da33d8c9504701
Trishant Patidar <trishantpatidar@gmail.com>
2026-05-26 15:44:17 +0530

    Upload src/main/java/com/example/bfhl/exception/InvalidInputException.java

---
+++ src/main/java/com/example/bfhl/exception/InvalidInputException.java
@@ -0,0 +1,7 @@
+package com.example.bfhl.exception;
+
+public class InvalidInputException extends RuntimeException {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
